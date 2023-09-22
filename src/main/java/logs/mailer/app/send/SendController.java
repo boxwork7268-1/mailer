59bcfd030b59a954f289a84623b9f744b7aea0e1
soft6dev <soft6dev@gmail.com>
2023-09-22 01:13:52 +0100

    best

--- src/main/java/logs/mailer/app/send/SendController.java
+++ src/main/java/logs/mailer/app/send/SendController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,8 @@ public class SendController {
 
 	@Autowired
 	private SendService sendService;
-	
-	@RequestMapping("/addsends")
+
+	@RequestMapping(method = RequestMethod.POST, value = "/addsends")
 	public void addSends(@RequestBody List<Send> sends) {
 		sendService.addSends(sends);
 	}
